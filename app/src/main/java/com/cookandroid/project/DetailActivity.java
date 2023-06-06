555223c5df82bd54d3692c9f7275b2dd38e445b1
Daseong <juniorteam11@gmail.com>
2023-06-06 09:56:37 +0900

    음식->식재료 불러오기 로직, 결과창 추가

--- app/src/main/java/com/cookandroid/project/DetailActivity.java
+++ app/src/main/java/com/cookandroid/project/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.cookandroid.project;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,24 +11,16 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
     private TextView searchResultTextView;
-    private List<String> Budaejjigae, JaeyukDeopbap, Rjjigae, Gimbap, CurryRice, Tteokbokki;
-
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        // 뒤로 가기 버튼을 눌렀을 때의 동작 처리
-        if (item.getItemId() == android.R.id.home) {
-            finish(); // 액티비티 종료
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    private List<String> ingredientsList;
+    private List<String> selectedIngredients;
+    private int[] buttonStates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,28 +35,66 @@ public class DetailActivity extends AppCompatActivity {
 
         // 검색어를 TextView에 설정
         searchResultTextView.setText(searchQuery);
-        // 뒤로 가기 버튼을 활성화
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // 음식에 따른 식재료 목록 설정
+        ingredientsList = getIngredientsList(searchQuery);
+
+        // 선택된 식재료를 저장할 리스트 초기화
+        selectedIngredients = new ArrayList<>();
 
         LinearLayout buttonContainer = findViewById(R.id.buttonContainer); // 버튼이 추가될 컨테이너
 
-        // 버튼 생성 및 속성 설정
-        Rjjigae = Arrays.asList("고추장","애호박","쇠고기","대파","마늘","양파","감자");
 
-        for (String ingredient : Rjjigae) {
+        for (String ingredient : ingredientsList) {
             Button button = new Button(this); // 버튼 생성
             button.setText(ingredient); // 버튼 텍스트 설정
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    // 버튼 클릭 이벤트 처리
-
-                    // 예시: 해당 재료에 대한 동작 수행
+                    toggleIngredient(button, ingredient);
                 }
             });
             buttonContainer.addView(button); // 버튼을 컨테이너에 추가
         }
+    }
 
+    // 음식에 따른 식재료 목록을 가져오는 메서드
+    private List<String> getIngredientsList(String food) {
+        List<String> ingredients = new ArrayList<>();
+
+        // 해당 음식에 따른 식재료 목록을 가져오는 로직
+        switch (food) {
+            case "부대찌개":
+                ingredients = Arrays.asList("김치", "비엔나", "스팸", "두부", "떡", "대파", "홍고추", "청양고추", "사골곰탕");
+                break;
+            case "제육덮밥":
+                ingredients = Arrays.asList("밥", "돼지고기", "양파", "당근", "표고", "대파", "청양고추", "홍고추");
+                break;
+            case "고추장찌개":
+                ingredients = Arrays.asList("고추장", "애호박", "쇠고기", "대파", "마늘", "양파", "감자");
+                break;
+            // 다른 음식에 대한 식재료 목록 설정
+        }
+
+        return ingredients;
+    }
+
+    // 식재료 토글 기능 처리
+    private void toggleIngredient(Button button, String ingredient) {
+        if (selectedIngredients.contains(ingredient)) {
+            selectedIngredients.remove(ingredient);
+        }
+    }
+
+    //뒤로가기
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // 뒤로 가기 버튼을 눌렀을 때의 동작 처리
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // 액티비티 종료
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
